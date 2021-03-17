8177d05b5ad9eadaa07a30fef142b5de1180024f
Gornovaya <79137339+Gornovaya@users.noreply.github.com>
2021-03-17 20:28:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,27 +1,27 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int* arr, int size, int value) {
-	int i = 0;
-	int j = size - 1;
-	int res = 0;
+int i = 0;
+int j = size - 1;
+int res = 0;
 
-	while (i < j) {
-		int mid = (i + j) / 2;
-		if (value < arr[mid])
-			j = mid;
-		else
-			i = mid+1;
-	}
+while (i < j) {
+int mid = (i + j) / 2;
+if (value < arr[mid])
+j = mid;
+else
+i = mid+1;
+}
 
-	if (value == arr[i]) {
-		while (arr[i] == value) {
-			res = res + 1;
-			i = i + 1;
-		}
-	}
+if (value == arr[i]) {
+while (arr[i] == value) {
+res = res + 1;
+i = i + 1;
+}
+}
 
-	if (res)
-		return res;
-	else
-		return 0;
+if (res)
+return res;
+else
+return 0;
 }
